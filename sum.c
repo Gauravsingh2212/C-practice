d3bc8f8d23ce4a7bf1594b44068a2ea1c4d4e2d6
Gauravsingh2212 <gauravsinghchouhan76@gmail.com>
2025-07-27 12:03:54 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+
+int main(){
+
+    //input
+    int a,b;
+    printf("Enter a number:");
+    scanf("%i",&a);
+    printf("Enter another number:");
+    scanf("%i",&b);
+    //sum
+    printf("sum is:%i",a+b);
+
+
+    return 0;
+}
